9156ed61452efd0352d4eb6b135619326013ba98
Maya Waleed <panda88626919@gmail.com>
2026-07-14 16:49:32 +0200

    Add Python projects

---
+++ monthes.py
@@ -0,0 +1,16 @@
+monthes = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+
+print (monthes["Mar"])
